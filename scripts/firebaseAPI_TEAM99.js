04c92115364d1e94b28f79ff8ffa923474bd1943
Samarjit Bhogal <samarjit.v.bhogal@gmail.com>
2024-03-05 10:01:21 -0800

    finshed parts 1 and 2

--- scripts/firebaseAPI_TEAM99.js
+++ scripts/firebaseAPI_TEAM99.js
@@ -2,13 +2,13 @@
 //  Your web app's Firebase configuration
 //----------------------------------------
 const firebaseConfig = {
-    apiKey: "AIzaSyDbuuuFu8G-j2G3SBkPLLqOjIe8-9B-Nlg",
-    authDomain: "comp1800-2024-demo07.firebaseapp.com",
-    projectId: "comp1800-2024-demo07",
-    storageBucket: "comp1800-2024-demo07.appspot.com",
-    messagingSenderId: "939630844455",
-    appId: "1:939630844455:web:2755d6021cd5cea0c1cc02"
-  };
+  apiKey: "AIzaSyDbuuuFu8G-j2G3SBkPLLqOjIe8-9B-Nlg",
+  authDomain: "comp1800-2024-demo07.firebaseapp.com",
+  projectId: "comp1800-2024-demo07",
+  storageBucket: "comp1800-2024-demo07.appspot.com",
+  messagingSenderId: "939630844455",
+  appId: "1:939630844455:web:2755d6021cd5cea0c1cc02"
+};
 
 //--------------------------------------------
 // initialize the Firebase app
